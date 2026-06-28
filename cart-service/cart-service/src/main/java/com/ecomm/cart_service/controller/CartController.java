8f79e0d126cb27937781797589e8d1fac09872d7
hekanth <hekanth5597@gmail.com>
2026-06-28 22:11:39 +0530

    "Implemented cart service post api"

---
+++ cart-service/cart-service/src/main/java/com/ecomm/cart_service/controller/CartController.java
@@ -0,0 +1,25 @@
+package com.ecomm.cart_service.controller;
+
+import com.ecomm.cart_service.dto.CartRequest;
+import com.ecomm.cart_service.entity.CartItem;
+import com.ecomm.cart_service.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<CartItem> saveCart(@RequestBody CartRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartService.saveCart(request));
+    }
+}
